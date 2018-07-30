b03cc7656f1ef8ef87d106bba583ffc1e5528fd7
GamingFTW <GamingFTW@mail.com>
2018-07-30 00:33:29 -0400

    DaysSinceHZ Episode
    Displays the number of days since the last Hanazuki Episode.

---
+++ dayssincehz/dayssincehz.py
@@ -0,0 +1,32 @@
+import datetime
+from datetime import date
+import discord
+from discord.ext import commands
+
+
+class DaysSinceHz:
+    """Simple display of number days since the last Hanazuki episode."""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def dayssincehz(self):
+        """Display days since the last Hanzuki Episode"""
+
+        now = datetime.datetime.now()
+        today = date(now.year, now.month, now.day)
+
+        year = now.year
+        if (now.month == 7 and now.day > 13):
+            year = now.year + 1
+
+        dayssincehz = date(2017, 7, 13)
+
+        delta = today - dayssincehz
+
+        await self.bot.say("```" + str(delta.days) + " days days since last Hanazuki Episode!```")
+
+
+def setup(bot):
+    bot.add_cog(DaysSinceHz(bot))
